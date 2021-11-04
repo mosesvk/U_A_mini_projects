0092c9530322dab545f9343d62d1a018a159c7f4
mosesvk <kaumatule93@gmail.com>
2021-11-04 17:48:19 -0600

    update

--- project-section-10-useEffects/src/components/Login/Login.js
+++ project-section-10-useEffects/src/components/Login/Login.js
@@ -40,11 +40,12 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: '',
+    value: "",
     isValid: null,
   });
+
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
-    value: '',
+    value: "",
     isValid: null,
   });
 
@@ -72,25 +73,31 @@ const Login = (props) => {
   }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+    dispatchEmail({
+      type: "USER_INPUT",
+      val: event.target.value,
+    });
 
     // setFormIsValid(
-    //   event.target.value.includes('@') && passwordState.isValid
+    //   event.target.value.trim().length > 6 && passwordState.isValid
     // );
   };
 
   const passwordChangeHandler = (event) => {
-    dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
+    dispatchPassword({
+      type: "USER_INPUT",
+      val: event.target.value,
+    });
 
-    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    // setFormIsValid(event.target.value.trim().length > 6 && emailState.isValid);
   };
 
-  const validateEmailHandler = () => {
-    dispatchEmail({ type: 'INPUT_BLUR' });
+  const validateEmailHandler = (state, action) => {
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({ type: 'INPUT_BLUR' });
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
