84dffaf655d5776aad04b001b2c381c01980cb45
mosesvk <kaumatule93@gmail.com>
2021-11-05 11:41:31 -0600

    update

--- project-section-10-useEffects/src/store/auth-context.js
+++ project-section-10-useEffects/src/store/auth-context.js
@@ -2,25 +2,32 @@ import React, { createContext, useState } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
-  onLogout: () => {}
-})
+  onLogout: () => {},
+  onLogin: (email, password) => {}
+});
 
 export const AuthContextProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logoutHandler = () => {
-    setIsLoggedIn(false)
-  }
+    setIsLoggedIn(false);
+  };
 
   const loginHandler = () => {
-    setIsLoggedIn(true)
-  }
+    setIsLoggedIn(true);
+  };
 
   return (
-  <AuthContext.Provider>
-    {props.children}
-  </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
