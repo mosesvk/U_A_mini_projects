e46f3edf2634fe9df3b031ac9ca8dd3271707567
mosesvk <kaumatule93@gmail.com>
2021-11-05 11:48:18 -0600

    update

--- project-section-10-useEffects/src/App.js
+++ project-section-10-useEffects/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,14 +6,14 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-  
+  const ctx = useContext(AuthContext)
 
   return (
     <React.Fragment>
       <MainHeader/>
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
