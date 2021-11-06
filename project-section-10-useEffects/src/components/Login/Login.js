9f60861215dc501dd0b3141cf86ca0a661572803
mosesvk <kaumatule93@gmail.com>
2021-11-06 10:42:45 -0600

    update

--- project-section-10-useEffects/src/components/Login/Login.js
+++ project-section-10-useEffects/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.scss";
@@ -53,6 +53,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef(); 
+  const passwordInputRef = useRef()
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -109,13 +112,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      
+    } else {
+
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -125,6 +135,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
