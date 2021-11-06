87497b930a82e050bacaf254bf2b74b76d9a73ba
mosesvk <kaumatule93@gmail.com>
2021-11-05 20:34:08 -0600

    update

--- project-section-10-useEffects/src/components/Input/Input.js
+++ project-section-10-useEffects/src/components/Input/Input.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import Classes from './Input.module.scss'
+
 const Input = (props) => {
   return (
-    <div>
-      
-    </div>
+    <div
+    className={`${classes.control} ${
+      emailIsValid === false ? classes.invalid : ""
+    }`}
+  >
+    <label htmlFor="email">E-Mail</label>
+    <input
+      type="email"
+      id="email"
+      value={emailState.value}
+      onChange={emailChangeHandler}
+      onBlur={validateEmailHandler}
+    />
+  </div>
   )
 }
 
