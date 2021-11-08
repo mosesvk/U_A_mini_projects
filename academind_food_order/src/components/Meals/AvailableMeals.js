cd0a23ff3dbce03c7ba05cb431a90ae3e279bf3f
mosesvk <kaumatule93@gmail.com>
2021-11-08 15:42:04 -0700

    update

--- academind_food_order/src/components/Meals/AvailableMeals.js
+++ academind_food_order/src/components/Meals/AvailableMeals.js
@@ -1,45 +1,51 @@
-import React from 'react'
+import React from "react";
 
-import Card from '../UI/Card';
-import classes from './AvailableMeals.module.scss'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.scss";
 
 const DUMMY_ITEMS = [
   {
-    id: 'm1',
-    name: 'Beef Mushroom',
-    description: '100% Beef with mushroom and sauce',
+    id: "m1",
+    name: "Beef Mushroom",
+    description: "100% Beef with mushroom and sauce",
     price: 5.99,
   },
   {
-    id: 'm2',
-    name: 'the Cowboy',
-    description: 'Beef & Swiss',
+    id: "m2",
+    name: "the Cowboy",
+    description: "Beef & Swiss",
     price: 5.99,
   },
   {
-    id: 'm3',
-    name: 'Nutella Banana',
-    description: 'Nutella with Banana',
+    id: "m3",
+    name: "Nutella Banana",
+    description: "Nutella with Banana",
     price: 3.99,
   },
   {
-    id: 'm4',
-    name: 'Pineapple',
-    description: 'Fresh Pineapple',
+    id: "m4",
+    name: "Pineapple",
+    description: "Fresh Pineapple",
     price: 3.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const itemsList = DUMMY_ITEMS.map(item => <li>{item.name}</li>)
+  const itemsList = DUMMY_ITEMS.map((item) => (
+    <MealItem
+      key={item.id}
+      name={item.name}
+      description={item.description}
+      price={item.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <Card>
-        {itemsList}
-      </Card>
+      <Card>{itemsList}</Card>
     </section>
-  )
-}
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
