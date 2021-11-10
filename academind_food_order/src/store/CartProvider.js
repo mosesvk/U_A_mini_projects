1c82f2c646483658a951884362ee767cf212f67a
mosesvk <kaumatule93@gmail.com>
2021-11-09 21:19:07 -0700

    update

--- academind_food_order/src/store/CartProvider.js
+++ academind_food_order/src/store/CartProvider.js
@@ -3,20 +3,23 @@ import CartContext from './cart-context'
 
 const defaultCartState = {
   items: [],
-  totalamount: 0
+  totalAmount: 0
 };
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     const updatedItems = state.items.concat(action.item)
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
-    return {}
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
   }
 
 }
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(useReducer, defaultCartState)
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
   const addItemToCartHandler = (item) => {
     dispatchCartAction({type: 'ADD', item: item})
