dacd6b575cd2b1885947f6a2b74d58fb3aab0ebe
mosesvk <kaumatule93@gmail.com>
2021-11-11 11:52:09 -0700

    update

--- academind_food_order/src/store/CartProvider.js
+++ academind_food_order/src/store/CartProvider.js
@@ -36,12 +36,10 @@ const cartReducer = (state, action) => {
 
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
-      item => item.id == action.item.id
+      (item) => item.id === action.id
     );
-
     const existingItem = state.items[existingCartItemIndex]
     const updatedTotalAmount = state.totalAmount - existingItem.price
-
     let updatedItems;
     if (existingItem.amount === 1 ) {
       updatedItems = state.items.filter(item => item.id !== action.id)
@@ -50,13 +48,12 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem
     }
-
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     }
 
-  }
+  }// ---------- if 'REMOVE' ------->
 
 
   return defaultCartState;
