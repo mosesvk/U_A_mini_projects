eb5f51a4abbe8d83c02d6e22a3506a1299099c10
mosesvk <kaumatule93@gmail.com>
2021-11-15 17:39:37 -0700

    update

--- 14-httpRequest/src/App.js
+++ 14-httpRequest/src/App.js
@@ -30,7 +30,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <h2>No Movies Updated</h2>}
         {isLoading && <h2>Loading...</h2>}
       </section>
     </React.Fragment>
