a44df9ca31a92a07b06afaa778157272b50ba3bd
mosesvk <kaumatule93@gmail.com>
2021-11-16 08:34:26 -0700

    update

---
+++ 15-customHooksRealistic/src/hooks/use-http.js
@@ -0,0 +1,39 @@
+const {useState, useEffect}
+
+const useHttp = (requestConfig) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        requestConfig.url, {
+          method: requestConfig.method,
+          headers: requestConfig.headers,
+          body: JSON.stringify(requestConfig.body)
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+}
+
+export default useHttp
