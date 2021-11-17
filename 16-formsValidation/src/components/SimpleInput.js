7ca21348326c814c81eb6e7d633be816b9e1c571
mosesvk <kaumatule93@gmail.com>
2021-11-17 05:46:41 -0700

    update

--- 16-formsValidation/src/components/SimpleInput.js
+++ 16-formsValidation/src/components/SimpleInput.js
@@ -1,9 +1,21 @@
+import { useState, useRef } from 'react'
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // This stops the browsers default behavior in submitting a form which is sending an HTTP request to a server (which we don't have at the moment)
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
