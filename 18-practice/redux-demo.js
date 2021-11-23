001a40df4ced50b20a0d6f5675e3e9da7e23bedf
mosesvk <kaumatule93@gmail.com>
2021-11-22 19:21:48 -0700

    update

--- 18-practice/redux-demo.js
+++ 18-practice/redux-demo.js
@@ -1,16 +1,20 @@
-const redux = require('redux')
+const redux = require("redux");
 
-const counterReducer = (state = {counter: 0}, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   return {
-    counter: state.counter + 1
-  }
-}
+    counter: state.counter + 1,
+  };
+};
 
-redux.createStore(counterReducer)
+const store = redux.createStore(counterReducer);
+
+console.log(store.getState());
 
 const counterSubscriber = () => {
-  const latestState = store.getState()
-  console.log(latestState)
-}
+  const latestState = store.getState();
+  console.log(latestState);
+};
 
-store.subscribe(counterSubscriber)
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "INCREMENT" });
