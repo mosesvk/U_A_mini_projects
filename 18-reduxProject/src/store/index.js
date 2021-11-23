2dfe76cda5b3f71e456a176e7cd93f8c6e976052
mosesvk <kaumatule93@gmail.com>
2021-11-23 07:08:50 -0700

    update

--- 18-reduxProject/src/store/index.js
+++ 18-reduxProject/src/store/index.js
@@ -1,19 +1,30 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {counter: 0, showCounter: true}
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT")
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   if (action.type === "INCREASE") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter
     };
   }
-  if (action.type === "DECREMENT") {
+  if (action.tdype === "DECREMENT") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter
     };
+  }
+  if (action.type === "TOGGLE") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter
+    }
   }
 
   return state;
