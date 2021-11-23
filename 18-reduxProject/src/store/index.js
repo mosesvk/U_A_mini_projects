4e74e8f6fd9dc5ff426af490a4f1e4ba6ae8ba37
mosesvk <kaumatule93@gmail.com>
2021-11-23 13:09:42 -0700

    update

--- 18-reduxProject/src/store/index.js
+++ 18-reduxProject/src/store/index.js
@@ -1,36 +1,56 @@
 import { createStore } from "redux";
+import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {counter: 0, showCounter: true}
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT")
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  if (action.type === "INCREASE") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
-  if (action.tdype === "DECREMENT") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-  if (action.type === "TOGGLE") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
+const counterSlice = createSlice({
+  name: 'counter', 
+  initialState, 
+  reducers: {
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount
+    }, 
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
     }
   }
+})
 
-  return state;
-};
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "INCREMENT")
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter
+//     };
+//   if (action.type === "INCREASE") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter
+//     };
+//   }
+//   if (action.type === "DECREMENT") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter
+//     };
+//   }
+//   if (action.type === "TOGGLE") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter
+//     }
+//   }
 
-const store = createStore(counterReducer);
+//   return state;
+// };
+
+const store = createStore(counterSlice.reducer);
 
 export default store;
 
