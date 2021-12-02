eaf598d42c8f812fd180ba3133942235b4f6a92c
mosesvk <kaumatule93@gmail.com>
2021-12-02 12:45:36 -0700

    update

--- 19-reduxAdvanced/src/store/ui-slice.js
+++ 19-reduxAdvanced/src/store/ui-slice.js
@@ -1,5 +1,15 @@
 import {createSlice} from '@reduxjs/toolkit'
 
-createSlice({
-  
+const uiSlice = createSlice({
+  name: 'ui', 
+  initialState: {cartIsVisible: false},
+  reducers: {
+    toggle(state){
+      state.cartIsVisible = !state.cartIsVisible
+    }
+  }
 })
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice
