ce5fd8e49e806303e53723e853e04b0ea167737e
mosesvk <kaumatule93@gmail.com>
2021-12-02 19:38:39 -0700

    update

--- 19-reduxAdvanced/src/store/indexStore.js
+++ 19-reduxAdvanced/src/store/indexStore.js
@@ -1,1 +1,9 @@
-import {createSlice, configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+
+import uiSlice  from "./ui-slice";
+
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
+
+export default store;
