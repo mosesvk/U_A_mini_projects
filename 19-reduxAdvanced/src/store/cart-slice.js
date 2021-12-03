2ea76e156ecbd5535fc0171970e0f26eac0a4f1c
mosesvk <kaumatule93@gmail.com>
2021-12-02 21:56:45 -0700

    update;

--- 19-reduxAdvanced/src/store/cart-slice.js
+++ 19-reduxAdvanced/src/store/cart-slice.js
@@ -1,2 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {value: 18, showCounter: true}
+createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.item.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.item.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+      }
+    },
+    removeItemToCart() {},
+  },
+});
