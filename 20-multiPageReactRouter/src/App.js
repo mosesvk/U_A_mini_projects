8dcfbd748ceffd810d859276939b4b8e8630ae33
mosesvk <kaumatule93@gmail.com>
2021-12-04 09:18:21 -0700

    update

--- 20-multiPageReactRouter/src/App.js
+++ 20-multiPageReactRouter/src/App.js
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <>
-      <header></header>
+      <MainHeader/>
       <main>
         <Route path="/welcome" component={Welcome} />
         <Route path="/products" component={Products} />
