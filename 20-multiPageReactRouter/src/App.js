e115692c85281a9db818a35a47f136fdc1776735
mosesvk <kaumatule93@gmail.com>
2021-12-04 05:46:33 -0700

    udpate

--- 20-multiPageReactRouter/src/App.js
+++ 20-multiPageReactRouter/src/App.js
@@ -1,10 +1,13 @@
 import {Route} from 'react-router-dom '
+import Products from './components/Products';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Route path='/welcome' component={Welcome}/>
+      <Route path='/products' component={Products}/>
+    </>
   );
 }
 
