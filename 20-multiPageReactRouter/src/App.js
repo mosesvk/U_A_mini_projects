d5422066680ad87e5a6769261a07e82ca8a908ba
mosesvk <kaumatule93@gmail.com>
2021-12-04 05:47:59 -0700

    update

--- 20-multiPageReactRouter/src/App.js
+++ 20-multiPageReactRouter/src/App.js
@@ -1,12 +1,12 @@
-import {Route} from 'react-router-dom '
-import Products from './components/Products';
-import Welcome from './components/Welcome';
+import { Route } from "react-router-dom ";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <>
-      <Route path='/welcome' component={Welcome}/>
-      <Route path='/products' component={Products}/>
+      <Route path="/welcome" component={Welcome} />
+      <Route path="/products" component={Products} />
     </>
   );
 }
