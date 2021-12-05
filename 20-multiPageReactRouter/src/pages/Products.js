2be1c70e754c5076cf04005e28f6fb7334f5aebf
mosesvk <kaumatule93@gmail.com>
2021-12-04 20:56:26 -0700

    update

--- 20-multiPageReactRouter/src/pages/Products.js
+++ 20-multiPageReactRouter/src/pages/Products.js
@@ -1,13 +1,13 @@
-import {NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Products = () => {
   return (
     <section>
       <h1>Products Page</h1>
       <ul>
-        <NavLink to='/product-detail/anything'>Product 1</NavLink>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <Link to='/products/p1'>Product 1</Link>
+        <Link to='/products/p2'>Product 2</Link>
+        <li to='/products/p3'>Product 3</li>
       </ul>
     </section>
   )
