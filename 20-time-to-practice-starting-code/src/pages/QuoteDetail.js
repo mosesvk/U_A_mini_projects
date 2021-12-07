205ad2e0503445f0cfe6a6e1d6b70e540f5a30f4
mosesvk <kaumatule93@gmail.com>
2021-12-07 10:52:59 -0700

    update

--- 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import {useParams} from 'react-router-dom'
 
 const QuoteDetail = () => {
+  const params = useParams();
+
   return (
-    <div>
+    <>
       <h1>Quote Detail Page</h1>
-    </div>
+      {params.quoteId}
+    </>
   )
 }
 
