76f529e73331c7512d4aab36e7d0f3fa253aa0d0
mosesvk <kaumatule93@gmail.com>
2021-12-06 21:46:51 -0700

    udpate

--- 20-time-to-practice-starting-code/src/App.js
+++ 20-time-to-practice-starting-code/src/App.js
@@ -1,8 +1,12 @@
+import {Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/quotes' exact/>
+      <Route path='/quotes/:quoteId'/>
+      <Route path='/new-quote'/>
+    </Switch>
   );
 }
 
