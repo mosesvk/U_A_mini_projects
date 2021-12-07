6f9789883841e035d91153924d83617967e60fe4
mosesvk <kaumatule93@gmail.com>
2021-12-06 22:29:44 -0700

    update

--- 20-time-to-practice-starting-code/src/App.js
+++ 20-time-to-practice-starting-code/src/App.js
@@ -1,11 +1,15 @@
 import {Route, Switch} from 'react-router-dom'
 
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
     <Switch>
-      <Route path='/quotes' exact/>
-      <Route path='/quotes/:quoteId'/>
-      <Route path='/new-quote'/>
+      <Route path='/quotes' exact component={AllQuotes}/>
+      <Route path='/quotes/:quoteId' component={QuoteDetail}/>
+      <Route path='/new-quote' component={NewQuote}/>
     </Switch>
   );
 }
