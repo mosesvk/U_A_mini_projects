8c19376fb414895904dbd5d5fb9126525563b098
mosesvk <kaumatule93@gmail.com>
2021-12-06 22:50:02 -0700

    update

--- 20-time-to-practice-starting-code/src/App.js
+++ 20-time-to-practice-starting-code/src/App.js
@@ -7,10 +7,10 @@ import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
-      <Route path='/' exact>
-        <Redirect path='/quotes' />
+      <Route path="/" exact>
+        <Redirect path="/quotes" />
       </Route>
-      <Route path="/quotes" exact component={AllQuotes} />
+      <Route path="/quotes" component={AllQuotes} exact/>
       <Route path="/quotes/:quoteId" component={QuoteDetail} />
       <Route path="/new-quote" component={NewQuote} />
     </Switch>
