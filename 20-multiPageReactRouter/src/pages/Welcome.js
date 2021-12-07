a10b18c3828e42bc42cab159316b082292ce1af3
mosesvk <kaumatule93@gmail.com>
2021-12-06 17:48:10 -0700

    update

--- 20-multiPageReactRouter/src/pages/Welcome.js
+++ 20-multiPageReactRouter/src/pages/Welcome.js
@@ -1,9 +1,12 @@
-
+import {Route} from 'react-router-dom'
 
 const Welcome = () => {
   return (
     <>
       <h1>Welcome Page</h1>
+      <Route path='/welcome/new-user'>
+        <h1>Welcome New User!</h1>
+      </Route>
     </>
   )
 }
