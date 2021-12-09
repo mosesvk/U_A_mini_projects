255436c967c8eb3efeff6aff348f0b5dba01066b
mosesvk <kaumatule93@gmail.com>
2021-12-09 12:39:39 -0700

    update

--- 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 20-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,17 +1,18 @@
 import { useParams, Route, Link } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
+import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_DATA = [
-  {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-  {id: 'q2', author: 'Maximilian', text: 'Learning React is great'}
-]
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  console.log(params)
 
-  const quote = DUMMY_DATA.find(quote => quote.id === params.quoteId)
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
     return (
@@ -19,16 +20,20 @@ const QuoteDetail = () => {
         <Link to={`/quotes`}>Back</Link>
         <p>No Quote Found</p>
       </>
-    )
+    );
   }
 
   return (
     <>
-      <Link to='/quotes'>Back</Link>
+      <Link to="/quotes">Back</Link>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <div className='centered'>
-        <Link className='btn--flat' to={`/quotes/${params.quotId}/comments`}>Load Comments</Link>
-      </div>
+      <Route path={`/quotes/${params.quotId} exact`}>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quotId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quotId}/comments`} component={Comments} />
     </>
   );
