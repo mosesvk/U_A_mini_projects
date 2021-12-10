4be9262b581faca1c19bec9821a3138991d991c5
mosesvk <kaumatule93@gmail.com>
2021-12-09 20:47:56 -0700

    update

--- 20-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 20-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const DUMMY_DATA = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
@@ -6,6 +11,33 @@ const DUMMY_DATA = [
 ];
 
 const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest()
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0))
+
+
+
   return <QuoteList quotes={DUMMY_DATA} />;
 };
 
