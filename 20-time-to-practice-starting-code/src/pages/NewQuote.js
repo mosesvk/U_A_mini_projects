cf45a4ff207a97be257e284d409a1e8138172570
mosesvk <kaumatule93@gmail.com>
2021-12-09 20:20:11 -0700

    update

--- 20-time-to-practice-starting-code/src/pages/NewQuote.js
+++ 20-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router";
 
-import QuoteForm from "../components/quotes/QuoteForm"
+import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
-
-    history.push('/quotes')
-  }
+    sendRequest(quoteData);
+  };
 
   return (
-    <QuoteForm onAddQuote={addQuoteHandler} />
-  )
-}
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
+};
 
-export default NewQuote
+export default NewQuote;
