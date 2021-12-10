be61bb4004485f6300c536e1da3f6270e8061466
mosesvk <kaumatule93@gmail.com>
2021-12-09 20:53:38 -0700

    update

--- 20-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 20-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -2,13 +2,10 @@ import { useEffect } from "react";
 
 import QuoteList from "../components/quotes/QuoteList";
 import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
 import useHttp from "../hooks/use-http";
 import { getAllQuotes } from "../lib/api";
 
-const DUMMY_DATA = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great" },
-];
 
 const AllQuotes = () => {
   const {
@@ -34,11 +31,13 @@ const AllQuotes = () => {
     return <p className='centered focused'>{error}</p>
   }
 
-  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0))
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />
+  }
 
 
 
-  return <QuoteList quotes={DUMMY_DATA} />;
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
