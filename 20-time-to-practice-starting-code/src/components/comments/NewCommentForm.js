42befce2bb7d44c8a412685d7205e568286fbab3
mosesvk <kaumatule93@gmail.com>
2021-12-13 21:35:58 -0700

    update

--- 20-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
+++ 20-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 import useHttp from '../../hooks/use-http';
 import { addComment } from '../../lib/api';
@@ -9,6 +9,10 @@ const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
   const {sendRequest, status} = useHttp(addComment)
+
+  useEffect(() => {
+
+  }, [])
 
   const submitFormHandler = (event) => {
     event.preventDefault();
