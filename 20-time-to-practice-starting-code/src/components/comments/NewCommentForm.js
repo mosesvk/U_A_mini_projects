11f57d53173c02795c7b45bdff84c6405354f847
mosesvk <kaumatule93@gmail.com>
2021-12-13 21:31:25 -0700

    update

--- 20-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
+++ 20-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 
 import useHttp from '../../hooks/use-http';
 import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './NewCommentForm.module.css';
 
 const NewCommentForm = (props) => {
@@ -13,13 +14,18 @@ const NewCommentForm = (props) => {
     event.preventDefault();
 
     // optional: Could validate here
+    const enteredText = commentTextRef.current.value
 
-    // send comment to server
-    sendRequest();
+    sendRequest({text: enteredText});
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className='centered'>
+          <LoadingSpinner/>
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
