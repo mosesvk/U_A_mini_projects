4a99e67e3abb40bdeb7869fb180631d4e42f64f4
mosesvk <kaumatule93@gmail.com>
2021-12-13 21:54:14 -0700

    update

--- 20-time-to-practice-starting-code/src/components/comments/Comments.js
+++ 20-time-to-practice-starting-code/src/components/comments/Comments.js
@@ -1,32 +1,55 @@
-import { useState } from 'react';
-import { useParams } from 'react-router';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const Comments = () => {
   const params = useParams();
-  console.log(params)
-  const quoteId = params.quoteId
+  const quoteId = params.quoteId;
   const [isAddingComment, setIsAddingComment] = useState(false);
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
 
-  const addedCommentHandler = () => {
+  const addedCommentHandler = () => {};
 
+  let comments;
+
+  if (status === "params") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
   }
+
   
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
       <p>Comments...</p>
     </section>
   );
