bef5046fa6ec061c45aa009127c8af6d92e5e300
mosesvk <kaumatule93@gmail.com>
2021-12-15 14:11:54 -0700

    update

--- 20-time-to-practice-starting-code/src/components/comments/CommentsList.js
+++ 20-time-to-practice-starting-code/src/components/comments/CommentsList.js
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
-
 import CommentItem from "./CommentItem";
 import classes from "./CommentsList.module.css";
-import useHttp from "../../hooks/use-http";
-import { removeComment } from "../../lib/api";
 
 const CommentsList = (props) => {
   const { onRemoveComment } = props;
-  const { sendRequest, status, error } = useHttp(removeComment);
-
-  useEffect(() => {
-    if (status === "completed" && !error) {
-      onRemoveComment();
-    }
-  }, [status, error, onRemoveComment]);
 
   return (
     <ul className={classes.comments}>
