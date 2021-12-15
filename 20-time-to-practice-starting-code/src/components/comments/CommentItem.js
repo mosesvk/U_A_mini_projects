3f34908296bc350e7d4ddb1c28c829364557151a
mosesvk <kaumatule93@gmail.com>
2021-12-15 11:53:57 -0700

    update

--- 20-time-to-practice-starting-code/src/components/comments/CommentItem.js
+++ 20-time-to-practice-starting-code/src/components/comments/CommentItem.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import classes from "./CommentItem.module.css";
 import useHttp from "../../hooks/use-http";
 import { removeComment } from "../../lib/api";
@@ -6,15 +8,22 @@ const CommentItem = (props) => {
   const { sendRequest, status, error } = useHttp(removeComment);
   const { removeComment } = props;
 
+  useEffect(() => {
+    if (status === 'completed' && !error){
+      removeComment();
+    }
+  }, [status, error, removeComment])
+
   const removeCommentHandler = (event) => {
     event.preventDefault();
+    console.log(props.id)
     sendRequest({ quoteId: props.id });
   };
 
   return (
     <li className={classes.item}>
       <p>{props.text}</p>
-      <button onClick={props.removeComment}>Delete</button>
+      <button onClick={removeCommentHandler}>Delete</button>
     </li>
   );
 };
