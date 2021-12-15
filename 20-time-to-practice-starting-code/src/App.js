07b34dab6a6628f13bded6bb2431d47db0c97fbe
mosesvk <kaumatule93@gmail.com>
2021-12-14 22:09:31 -0700

    update

--- 20-time-to-practice-starting-code/src/App.js
+++ 20-time-to-practice-starting-code/src/App.js
@@ -5,21 +5,30 @@ import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 const NewQuote = React.lazy(() => import("./pages/NewQuote"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" component={AllQuotes} exact />
-        <Route path="/quotes/:quoteId" component={QuoteDetail} />
-        <Route path="/new-quotes" component={NewQuote} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" component={AllQuotes} exact />
+          <Route path="/quotes/:quoteId" component={QuoteDetail} />
+          <Route path="/new-quotes" component={NewQuote} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
