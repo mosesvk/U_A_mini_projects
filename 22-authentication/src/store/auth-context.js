fc1fc35d455131fcc05981a70bf3c9cf906648aa
mosesvk <kaumatule93@gmail.com>
2021-12-18 18:44:01 -0700

    update

--- 22-authentication/src/store/auth-context.js
+++ 22-authentication/src/store/auth-context.js
@@ -1,16 +1,37 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
-  token: '', 
+  token: "",
   isLoggedIn: false,
   login: (token) => {},
-  logout: () => {}
-})
+  logout: () => {},
+});
 
-const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null)
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
 
   const userIsLoggedIn = !!token;
 
-  return <AuthContext.Provider>{props.children}</AuthContext.Provider>
-}
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
