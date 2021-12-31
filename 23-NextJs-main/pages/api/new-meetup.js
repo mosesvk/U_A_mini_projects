68078c78c655700a68052ccae1d88f67dd0df178
mosesvk <kaumatule93@gmail.com>
2021-12-31 09:41:16 -0700

    update]

--- 23-NextJs-main/pages/api/new-meetup.js
+++ 23-NextJs-main/pages/api/new-meetup.js
@@ -1,11 +1,24 @@
 // api route ___ /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (req, res) => {
-  if (req.method === "POST") {
-    const data = req.body;
+import {MongoClient} from 'mongodb'
 
-    const { title, image, address, description } = data;
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body; 
+
+    const client = await MongoClient.connect('mongodb+srv://mosesvk:Lukifanga2656@cluster0.4gc6f.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db = client.db()
+
+    const meetupCollection = db.collection('meetups')
+
+    const result = await meetupsCollection.insertONe(data);
+
+    console.log(result)
+    
+    client.close();
+
+    res.status(201).json({message: 'Meetup inserted!'})
   }
 };
 
