2e290751342e6c71099456806cb67962cd58ad99
mosesvk <kaumatule93@gmail.com>
2021-12-31 15:47:18 -0700

    update]

--- 23-NextJs-main/.next/static/chunks/webpack.js
+++ 23-NextJs-main/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0146c058cf0aa077851a";
+/******/ 	var hotCurrentHash = "26a6ddacabcb124d204d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- 23-NextJs-main/pages/index.js
+++ 23-NextJs-main/pages/index.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
 import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+  <>
+    <Head>
+      <title>React Meetups</title>
+    </Head>
+    <MeetupList meetups={props.meetups} />
+  </>);
 };
 
 // this is instead of using useState and useEffect in the HomePage function. 
