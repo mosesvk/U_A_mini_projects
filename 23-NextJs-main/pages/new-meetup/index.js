d73066d8813e4eb31f3b9059d41a741db9e0afa3
mosesvk <kaumatule93@gmail.com>
2021-12-30 21:50:37 -0700

    update]

---
+++ 23-NextJs-main/pages/new-meetup/index.js
@@ -0,0 +1,14 @@
+// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData)
+  }
+
+  return (
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  )
+}
+
+export default NewMeetupPage
