a79c53d0b59a1539f0d7b10a37af5bb6a6711095
mosesvk <kaumatule93@gmail.com>
2022-01-03 20:22:09 -0700

    update]

---
+++ 26-unit-testing/src/components/Greeting.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Greeting from './Greeting'
+
+test('renders Hello World as a text', () => {
+  // Arrange
+  render(<Greeting /> )
+
+  // Act
+  // ...Nothing
+
+  // Assert
+  const helloWorldElement = screen.getByText('Hello World', {exact: false})
+  expect(helloWorldElement).toBeInTheDocument()
+}) 
