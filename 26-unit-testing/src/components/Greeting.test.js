1bb6bce600745c513a16150551d0408d4c1ed8bc
mosesvk <kaumatule93@gmail.com>
2022-01-03 21:17:03 -0700

    update]

--- 26-unit-testing/src/components/Greeting.test.js
+++ 26-unit-testing/src/components/Greeting.test.js
@@ -29,6 +29,18 @@ describe('Greeting Component', () => {
     const buttonElement = screen.getByRole('button')
     userEvent.click(buttonElement)
     //Assert
-    const outputElement = screen.getByText('good to see you', {exact: false})
+    const outputElement = screen.getByText('Changed!')
+    expect(outputElement).toBeInTheDocument()
+  })
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting/>)
+    //Act 
+    const buttonElement = screen.getByRole('button')
+    userEvent.click(buttonElement)
+    //Assert
+    const outputElement = screen.queryByText('good to see you', {exact: false})
+    expect(outputElement).not.toBeInTheDocument() 
   })
 })
