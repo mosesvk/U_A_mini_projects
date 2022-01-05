7b81c348f6d3ac3de06def996f421184c2fa0229
mosesvk <kaumatule93@gmail.com>
2022-01-05 14:22:37 -0700

    update

--- 27-typescript-starting/src/basics.ts
+++ 27-typescript-starting/src/basics.ts
@@ -8,3 +8,26 @@
     age = 12;
 
     let userName: string;
+
+    userName = 'Max'
+
+    let isInstructor: boolean;
+    
+    isInstructor = false
+
+  // Complex
+    let hobbies: string[];
+
+    hobbies = ['bball', 'cooking']
+
+    let person: {
+      name: string,
+      age: number
+    };
+
+    person = {
+      name: 'Max', 
+      age: 32
+    }
+
+
