b549658dd10ba8528ee7a5239ac25cc89cc9bc39
mosesvk <kaumatule93@gmail.com>
2022-02-04 16:37:20 -0700

    deleting todo functionality

--- _typescript/src/components/TodoItem.tsx
+++ _typescript/src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import classes from '../styles/TodoItem.module.css'
 
-const TodoItem: React.FC<{text: string}> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{text: string, onRemoveTodo: () => void}> = (props) => {
+  return <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>;
 };
 
 export default TodoItem;
