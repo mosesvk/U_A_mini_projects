60b99fe4fb6349ca329acd8191cd36eec5d5a719
mosesvk <kaumatule93@gmail.com>
2022-02-03 17:42:37 -0700

    adding Todos component

--- _typescript/src/App.tsx
+++ _typescript/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos';
 
 const App = () => {
   return (
     <div>
-
+      <Todos/>
     </div>
   );
 }
