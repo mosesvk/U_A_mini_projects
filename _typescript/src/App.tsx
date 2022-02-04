30d87a85f63eca9ed6350539e6e15c0e461ba4ce
mosesvk <kaumatule93@gmail.com>
2022-02-04 15:50:28 -0700

    working with function props

--- _typescript/src/App.tsx
+++ _typescript/src/App.tsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 const App = () => {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  const addTodoHandler = (todoText: string) = {
-    
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText)
+
+    // this is just to add  
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo)
+    })
   }
 
   return (
